699fdad4abf9943d737a830aae3e4f908614061b
Jake <jakehong0521@berkeley.edu>
2021-02-26 17:25:53 -0800

    removing / updating files and paths

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// style
+import 'bootstrap/dist/css/bootstrap.css'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
